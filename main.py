9cb39809ca389ccc5f9a4d58e190d1309352fb7a
mayash01 <sharonmaya64@gmail.com>
2026-02-12 13:47:27 +0200

    reading the dataframe inside the tkinter and adding the start new game button

--- main.py
+++ main.py
@@ -6,22 +6,52 @@ import random
 import pandas as pd
 
 class Connections(tk.Tk):
-
-    def __init__(self, words) :
+    def __init__(self,connections_read) :
         super().__init__()
         self.title('game')
         self.geometry('480x400+400+200')
-        self.words = words
+        self.connections_read = connections_read
         self.submitted = set()
         self.curr = []  
+        self.words = []
+        self.all_groups = {}
+        self.groups = {}
+        self.group_count = {}
+        self.result_text = []
 
+    def read_to_df(self):
+        for sheet_name, dataframe in connections_read.items():
+            self.all_groups[sheet_name] = dataframe.to_dict()
+        
+        return self.all_groups
+    
+    def select_words(self):
+        for key in self.all_groups:
+            color = key
+            name = next(iter(self.all_groups[key]))
+            group = []
+
+            for val in self.all_groups[color][name].values():
+                val_clean = str(val).replace(" ", "").upper()
+                group.append(val_clean)
+                self.groups[(name,color)] = group
+                self.group_count[(name,color)] = 0
+
+            del self.all_groups[color][name]
+        
+        self.words = [w for word in self.groups.values() for w in word]
+        print(self.words)
+        return self.words, self.group, self.group_count
+    
     def start_click(self,start_button):
+        self.read_to_df()
+        self.select_words()
         start_button.destroy()
         self.label1.config(image="")
         self.label.destroy()
-
+        
         self.circles = self.create_circles()
-        self.bottuns = self.create_buttons(self.words)
+        self.buttons = self.create_buttons(self.words)
 
     def Start_Screen(self):
         self.image = tk.PhotoImage(file="connections.png")
@@ -42,42 +72,40 @@ class Connections(tk.Tk):
         return start_btn, self.label1, self.label
     
     def click(self, btn, word):
-        if sum(group_count.values()) == 4: 
+        if sum(self.group_count.values()) == 4: 
             if btn['bg'] == "#5a594e":
-                for g in groups:
-                    if word in groups[g]:
-                        group_count[g] -= 1
+                for g in self.groups:
+                    if word in self.groups[g]:
+                        self.group_count[g] -= 1
                         self.curr.remove(word)
                 btn.configure(bg="#efefe6")
-                # print(group_count)
         
         else:
             if btn['bg'] == "#efefe6":
-                for g in groups:
-                    if word in groups[g]:
+                for g in self.groups:
+                    if word in self.groups[g]:
                         self.curr.append(word)
-                        group_count[g] += 1
+                        self.group_count[g] += 1
                 btn.configure(bg="#5a594e")
 
             elif btn['bg'] == "#5a594e":
-                for g in groups:
-                    if word in groups[g]:
+                for g in self.groups:
+                    if word in self.groups[g]:
                         self.curr.remove(word)
-                        group_count[g] -= 1
+                        self.group_count[g] -= 1
                 btn.configure(bg="#efefe6")
-            # print(group_count)
 
     def submission(self):
         flag = False
         name = ''
         group_lst = []
-        for g in group_count:
-            if group_count[g] == 4:
-                if tuple(groups[g]) not in self.submitted:
+        for g in self.group_count:
+            if self.group_count[g] == 4:
+                if tuple(self.groups[g]) not in self.submitted:
                     name = g
-                    group_lst = groups[g]
+                    group_lst = self.groups[g]
                     flag = True
-                    for w in groups[g]: 
+                    for w in self.groups[g]: 
                         for b in self.Buttons:
                             if b['text'] == w:
                                 b.destroy() 
@@ -98,28 +126,31 @@ class Connections(tk.Tk):
                     row -= 1
 
             print('correct!') 
-            # print(group_count, groups)
-            count = 5-len(groups)     
-            text = f"{g[0]} \n {", ".join(group_lst)}"
-            # text = f"{g} \n {", ".join(group_lst)}"
-            bt2 = Button(self, text=text , bg=g[1] ,activebackground = '#efefe6', focusthickness = 0,height=60)
-            # bt2 = Button(self, text=text , bg='#efefe6',activebackground = '#efefe6', focusthickness = 0,height=60)
-            bt2.grid(row = count, columnspan=4, sticky = 'nsew', padx = 6, pady = 6)
+            count = 5-len(self.groups)    
+            text = f"{g[0]} \n {', '.join(group_lst)}"
+            result_btn = Button(self, text=text , bg=g[1] ,activebackground = '#efefe6', focusthickness = 0,height=60)
+            result_btn.grid(row = count, columnspan=4, sticky = 'nsew', padx = 6, pady = 6)
+            self.result_text.append(result_btn)
             
-            del groups[name]
-            del group_count[name]
-            # if not groups:
-            #     print('end')
+            del self.groups[name]
+            del self.group_count[name]
+            if not self.groups:
+                self.clean_board()
+                print('end')
+
         else: 
-            if sum(group_count.values()) == 4:
+            if sum(self.group_count.values()) == 4:
                 if tuple(self.curr) not in self.submitted: 
                     self.submitted.add(tuple(self.curr))
                     self.mistake()
                     self.curr = []
 
                 else: 
-                    self.tried = tk.Label(self, text='Already guessed!',fg="white")
-                    self.tried.grid(row = 0, columnspan = 4,  sticky = 'nsew')
+                    guessed = tk.Label(self, text='Already guessed...',bg='white', fg="black")
+                    guessed.grid(row = 0, columnspan = 5,  sticky = 'nsew')
+                    window.after(1000 ,guessed.destroy)
+                    self.Deselect_all()
+
 
     def Shuffle(self):
         random.shuffle(self.Buttons)
@@ -140,43 +171,58 @@ class Connections(tk.Tk):
             if b['bg'] == "#5a594e":
                 b.configure(bg="#efefe6", command=lambda word = b['text'], b=b: self.click(b, word))
 
-            for g in group_count:
-                group_count[g] = 0
+            for g in self.group_count:
+                self.group_count[g] = 0
 
     def mistake(self):
         if len(self.circles) > 1:
             self.Deselect_all()
         else:
-            self.results()
-            # self.new_game()
+            self.results()            
+            self.clean_board()
             print('lost the game')
+
         self.canvas.delete(self.circles.pop())
         
+    def clean_board(self):
+        print('new game')
+        self.mistakes.destroy()
+        self.Deselect_btn.destroy()
+        self.Shuffle_btn.destroy()
+        self.submit_btn.destroy()
+        self.tried.destroy()
+
+        new_game_btn = Button(self, text='Start new game', bg='black',fg = 'white', activebackground = 'white', focusthickness = 0, width=100, height=45)
+        new_game_btn.configure(command=lambda b=new_game_btn: self.new_game_click(b))
+        new_game_btn.grid(row = 5, columnspan=2, sticky = 'nsew', padx = 6, pady = 6)
 
     def results(self):
         r = 1
-        for n in groups:
-            text = f"{n[0]} \n {", ".join(groups[n])}"
-            bt2 = Button(self, text=text, bg=n[1], activebackground = '#efefe6', focusthickness = 0)
-            bt2.grid(row = r, columnspan=4, sticky = 'nsew', padx = 6, pady = 6)
+        for n in self.groups:
+            text = f"{n[0]} \n {', '.join(self.groups[n])}"
+            result_btn = Button(self, text=text, bg=n[1], activebackground = '#efefe6', focusthickness = 0)
+            result_btn.grid(row = r, columnspan=4, sticky = 'nsew', padx = 6, pady = 6)
+            self.result_text.append(result_btn)
             r += 1 
+
+    def new_game_click(self,new_game_btn):
+        print('begin')
+        new_game_btn.destroy()
+        for bt in self.result_text:
+            bt.destroy()
+        for b in self.Buttons:
+            b.destroy()
+
+        self.submitted = set()
+        self.curr = []  
+        self.words = []
+        self.groups = {}
+        self.group_count = {}
+        self.result_text = []
         
-    def game_end(self):
-        if len(groups) == 0:
-            self.mistakes.destroy()
-    # def new_game(self):
-    #     self.submit_btn.destroy()
-    #     self.Deselect_btn.destroy()
-    #     self.Shuffle_btn.destroy()
-    #     self.circles.remove()
-    #     new_game_btn = Button(self, text='Start new game', bg='black',fg = 'white', activebackground = 'white', focusthickness = 0, width=100, height=45)
-    #     new_game_btn.configure(command=lambda b=new_game_btn: self.start_click(b))
-    #     new_game_btn.grid(row = 2, column=2,  sticky = 'nsew', padx = 6, pady = 6)
-    #     return new_game_btn
-    
-    # def start_again(self, words):
-    #     self.circles = self.create_circles()
-    #     self.bottuns = self.create_buttons(self.words)
+        self.select_words()
+        self.circles = self.create_circles()
+        self.buttons = self.create_buttons(self.words)
 
     def create_buttons(self, words):
         self.tried = tk.Label(self, text='Create four groups of four!',fg="white")
@@ -202,16 +248,17 @@ class Connections(tk.Tk):
         self.submit_btn = Button(self, text='Submit', bg='#ffffff',activebackground = '#ffffff',fg = 'black', focusthickness = 0, command=self.submission)
         self.submit_btn.place(x=300, y=350)
 
-        Deselect_btn = Button(self, text='Deselect All', bg='#ffffff',activebackground = '#ffffff',fg = 'black', focusthickness = 0, command=self.Deselect_all)
-        Deselect_btn.place(x=175, y=350)
+        self.Deselect_btn = Button(self, text='Deselect All', bg='#ffffff',activebackground = '#ffffff',fg = 'black', focusthickness = 0, command=self.Deselect_all)
+        self.Deselect_btn.place(x=175, y=350)
 
-        Shuffle_btn = Button(self, text='Shuffle', bg='#ffffff',activebackground = '#ffffff',fg = 'black', focusthickness = 0, command=self.Shuffle)
-        Shuffle_btn.place(x=78, y=350)
+        self.Shuffle_btn = Button(self, text='Shuffle', bg='#ffffff',activebackground = '#ffffff',fg = 'black', focusthickness = 0, command=self.Shuffle)
+        self.Shuffle_btn.place(x=78, y=350)
+
         return self.Buttons
 
     def create_circles(self):
         self.circles = []
-        self.canvas = tk.Canvas(window, width=100, height=25, highlightthickness=0)
+        self.canvas = tk.Canvas(self, width=100, height=25, highlightthickness=0)
         self.circles.append(self.canvas.create_oval(5, 5, 20, 20, outline="#5a594e", fill="#5a594e"))
         self.circles.append(self.canvas.create_oval(25, 5, 40, 20, outline="#5a594e", fill="#5a594e"))
         self.circles.append(self.canvas.create_oval(45, 5, 60, 20, outline="#5a594e", fill="#5a594e"))
@@ -220,20 +267,7 @@ class Connections(tk.Tk):
         return self.circles
 
 if __name__ == "__main__":
-
     connections_read = pd.read_excel('connections_groups.xlsx', sheet_name=None)
-
-    groups = {}
-    group_count = {}
-    for sheet_name, dataframe in connections_read.items():
-        connection = dataframe.sample(n=1, axis='columns')
-        name = list(connection.columns.values)[0]
-        groups[(name,sheet_name)] = list(connection[name].values)
-        group_count[(name,sheet_name)] = 0
-
-    words = [w for word in groups.values() for w in word]
-
-    window = Connections(words)
+    window = Connections(connections_read)
     start_button = window.Start_Screen()
-
     window.mainloop()
